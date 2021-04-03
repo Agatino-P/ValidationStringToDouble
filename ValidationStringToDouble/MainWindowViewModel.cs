5df015e1c6260eb30a334846b7f9767369add1cc
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2021-04-03 14:24:53 +0200

    Added INotifyDataErrorInfo and collecting of all errors into one single place, as well as tooltip Might want to creta a base class for this

--- ValidationStringToDouble/MainWindowViewModel.cs
+++ ValidationStringToDouble/MainWindowViewModel.cs
@@ -1,16 +1,26 @@
 ﻿using FluentValidation.Results;
 using GalaSoft.MvvmLight;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 
 namespace ValidationStringToDouble
 {
-    public class MainWindowViewModel : ViewModelBase
+    public class MainWindowViewModel : ViewModelBase, INotifyDataErrorInfo
     {
-        private double _numero; public double Numero { get => _numero; set { Set(() => Numero, ref _numero, value); }}
+        private double _numero; public double Numero { get => _numero; set { Set(() => Numero, ref _numero, value); } }
 
-        public ObservableCollection<string> Errors { get; private set; } = new ObservableCollection<string>();
+        private List<string> _allErrors = new List<string>();
+        public List<string>  AllErrors { get => _allErrors; set { Set(() => AllErrors, ref _allErrors, value); }}
+        
+
+        public Dictionary<string, List<string>> Errors { get; private set; } = new Dictionary<string, List<string>>();
 
         private string _text;
+
         public string Text
         {
             get => _text;
@@ -21,6 +31,8 @@ namespace ValidationStringToDouble
             }
         }
 
+        public bool HasErrors => Errors.Count > 0;
+
         private void tryUpdateNumero()
         {
             DoubleStringValidatingConverter validatingConverter = new DoubleStringValidatingConverter();
@@ -30,17 +42,42 @@ namespace ValidationStringToDouble
                 Numero = validatingConverter.ConvertedValue;
                 RaisePropertyChanged(nameof(Numero));
                 Errors.Clear();
+                RaiseErrorChangedForProperty(nameof(Text));
             }
             else
             {
-                Errors.Clear();
-                foreach (ValidationFailure error in validationResults.Errors)
+                if (Errors.ContainsKey(nameof(Text)))
                 {
-                    Errors.Add(error.ErrorMessage);
+                    Errors.Remove(nameof(Text));
                 }
+                Errors.Add(nameof(Text), validationResults.Errors.Select(e=>e.ErrorMessage).ToList());
+                RaiseErrorChangedForProperty(nameof(Text));
             }
         }
 
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (string.IsNullOrWhiteSpace(propertyName))
+                return AllErrors;
+
+            if (Errors.ContainsKey(propertyName))
+            {
+                return Errors[propertyName];
+            }
+            else
+            {
+                return Enumerable.Empty<string>();
+            }
+        }
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        protected void RaiseErrorChangedForProperty(string propertyName)
+        {
+            AllErrors=new List<string>(Errors.SelectMany(kvp => kvp.Value.Select(err => $"{kvp.Key}:{err}")));
+            RaisePropertyChanged(nameof(AllErrors));
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
 
         public MainWindowViewModel()
         {
